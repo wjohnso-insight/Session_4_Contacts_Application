5bef90da616e9c94d9ad8d5bf4ca7eb87de175ba
Will <will.johnston@insight.com>
2021-02-17 15:35:47 -0500

    Added Contacts Service to Contacts Component
    1. Imported Contact interface and Contacts Service
    2. Created empty contacts property
    3. Injected service
    4. Created getContacts() method to set contacts property to this.contactsService.getContacts()
    5. Wired contacts.getContacts() method to ngOnInit()

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Contact } from '../contact'
+import { ContactsService } from '../contacts.service'
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
+  
+  contacts?: Contact[]; //Defined by contactsService.getContacts()
 
-  constructor() { }
+  constructor(private contactsService: ContactsService) { } //Dependency Injection
 
+  getContacts(): void{
+    this.contacts = this.contactsService.getContacts();
+  }
+  
   ngOnInit(): void {
+    this.getContacts();
   }
 
 }
