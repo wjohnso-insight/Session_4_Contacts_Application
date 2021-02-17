27ddce0df963d8011c06ec61c5e31a8408463f86
Will <will.johnston@insight.com>
2021-02-17 17:56:24 -0500

    Generated Component
    1. Generated InitialCircle Component
    
    This component should display the first and last initials of a contact

---
+++ src/app/initial-circle/initial-circle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-initial-circle',
+  templateUrl: './initial-circle.component.html',
+  styleUrls: ['./initial-circle.component.css']
+})
+export class InitialCircleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
