46fde625e05b8dd08f11c773a8d0d912c87ce3ea
Will <will.johnston@insight.com>
2021-02-17 16:03:51 -0500

    Replaced <li> elements with ContactCard Components
    1. Updated Contacts iterator to pass contactInfo atribute to ContactCard
    2. Updated ContactCard class definition with contactInfo property and linked to Contacts template via @Input
    3. Updated ContactCard template to display contactInfo.foo
    
    Closes issue #4

--- src/app/contactcard/contactcard.component.ts
+++ src/app/contactcard/contactcard.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Contact } from '../contact'
 @Component({
   selector: 'app-contactcard',
   templateUrl: './contactcard.component.html',
   styleUrls: ['./contactcard.component.css']
 })
 export class ContactcardComponent implements OnInit {
-
+  
+  @Input() contactInfo!: Contact;
   constructor() { }
 
   ngOnInit(): void {
