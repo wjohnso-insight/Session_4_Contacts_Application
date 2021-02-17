aadc1eb486f619be10b140d692a0f70d551c6835
Will <will.johnston@insight.com>
2021-02-17 15:30:57 -0500

    Generated Component
    1. Generated Contacts Component
    
    To Do:
    
    Display ContactCard components for each contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
