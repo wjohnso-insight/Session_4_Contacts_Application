58a79419022150493b4fe6dcab4154c9f768f189
Will <will.johnston@insight.com>
2021-02-17 19:32:28 -0500

    Contact Card Styling
    1. Updated contact card styling with wrapper ids/classes
    2. Added wrapper ids/classes to Contact Card (CC) template
    3. Added additioanl Contact to 'Contacts' array @ Contacts.ts
    4. Added uppercase pipe to 'initials' property in Initial Circle tempalte
    5. Added Roboto 100 font weight to Google Fonts import in Global Style

--- src/app/contacts.ts
+++ src/app/contacts.ts
@@ -22,5 +22,28 @@ export const Contacts : Contact[] = [
             catchPhrase: "P = NP",
             bs: "Make cool stuff."
         }
+    },
+    {
+        id: 12,
+        name: 'Julia Hypes',
+        username: 'wjohnsto',
+        email: 'j.hypes0336@gmail.com',
+        address: {
+            street: 'Lilley Ave.',
+            suite: undefined,
+            city: 'Columbus',
+            zipcode: '43206',
+            geo: {
+                lat: "-37.3159",
+                lng: "81.1496"
+            }
+        },
+        phone: "1-812-343-1048 x0001",
+        website: "willjohnston.tech",
+        company:{
+            name: "Insight",
+            catchPhrase: "P = NP",
+            bs: "Make cool stuff."
+        }
     }
 ]
